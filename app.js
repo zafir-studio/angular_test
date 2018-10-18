95f494e7e1995d229cda826bd8af1f9c657ea329
zafir-studio <44253944+zafir-studio@users.noreply.github.com>
2018-10-18 09:04:37 +0200

    beta

---
+++ app.js
@@ -0,0 +1,64 @@
+angular.module('tutorialApp', ['ngAnimate','ngRoute'])
+
+	.config(function($routeProvider) {
+	  $routeProvider
+	    .when('/', { templateUrl: 'articles.html' })
+	    .when('/about', { template: '&Uuml;ber unsere Pizzeria' })
+	    .otherwise({ redirectTo: '/' });
+	})
+
+	.factory('Cart', function() {
+		
+	    var items = [];
+	    
+	    return {
+	    	
+	      getItems: function() {
+	        return items;
+	      },
+	      
+	      addArticle: function(article) {
+	        items.push(article);
+	      },
+	      
+	      sum: function() {
+	        return items.reduce(function(total, article) {
+	          return total + article.price;
+	        }, 0);
+	      }
+	      
+	    };
+	    
+	})
+	
+	.controller('ArticlesCtrl', function($scope, Cart){
+		
+		$scope.cart = Cart;
+		
+		$scope.articles = [
+		  { id: 1, name: "Pizza Vegetaria", price: 5 },
+		  { id: 2, name: "Pizza Salami",    price: 5.5 },
+		  { id: 3, name: "Pizza Thunfisch", price: 6 },
+		  { id: 4, name: "Pizza Hawaii", price: 0 }
+	    ];
+		
+	 })
+	
+	.controller('CartCtrl', function($scope, Cart){
+		$scope.cart = Cart;
+	})
+	
+	.directive('price', function(){
+	  return {
+	    restrict: 'E',
+	    scope: {
+	      value: '='
+	    },
+	    template: '<span ng-show="value == 0">kostenlos</span>' +
+	      '<span ng-show="value > 0">{{value | currency}}</span>'
+	  }
+	})
+
+;
+
+
---
+++ test.php
@@ -0,0 +1,40 @@
+<?php 
+
+?>
+
+<html ng-app="tutorialApp">
+<head>
+  <meta charset="utf8mb4" />
+  <title>AngularJS Tutorial</title>
+  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+  
+  <style>
+  
+  	.cart-item.ng-enter {
+	  -webkit-transition:0.5s linear all;
+	  transition:0.5s linear all;
+	  background-color: yellow;
+	}
+	.cart-item.ng-enter-active {
+	  background-color: white;
+	}
+  
+  </style>
+  
+</head>
+<body>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-animate.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-route.js"></script>
+  <script src="app.js"></script>
+  
+  	<p class="well">
+	  <a href="#/">Start</a> | <a href="#/about">&Uuml;ber</a>
+	</p>
+	<div ng-view></div>
+  
+  
+</body>
+</html>
+
+
